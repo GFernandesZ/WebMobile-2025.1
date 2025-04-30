37cfcbb972d972a4f83af070dde88ba4be30ec9e
GFernandesZ <g.f.z2012.gfz@gmail.com>
2025-04-30 15:43:20 -0300

    Atualizando banco de dados e Html

--- sistema/settings.py
+++ sistema/settings.py
@@ -119,9 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = '/static/'
-STATICFILES_DIR = [BASE_DIR/'sistema'/'static']
+# Caminho para os arquivos estáticos
+STATIC_URL = '/static/'
+
+# Diretório onde os arquivos estáticos serão coletados
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Diretórios adicionais para buscar arquivos estáticos
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
