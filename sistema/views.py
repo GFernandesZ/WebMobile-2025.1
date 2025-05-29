ac36640c3c53ef307dcb4f1dedff4ba9a3a91b56
GFernandesZ <g.f.z2012.gfz@gmail.com>
2025-05-28 23:43:49 -0300

    Teste de API

--- sistema/views.py
+++ sistema/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import redirect, render
 from django.views import View
 from django.contrib.auth import authenticate, login, logout
 from django.conf import settings
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 class Login(View): 
     def get(self, request):
@@ -31,3 +34,22 @@ class Logout(View):
     def get(self, request):
         logout(request)
         return redirect(settings.LOGIN_URL)
+    
+class LoginAPI(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data,
+            context={
+                'request': request
+            }
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'id': user.id,
+            'nome': user.first_name,
+            'email': user.email,
+            'token': token.key
+        })
